fa47e9e280f5881bfe5178d8f303072e56b234c6
Ismael Barzani <96747803+TheBarzani@users.noreply.github.com>
2022-10-05 10:22:40 -0400

    COEN243 Tutorial 3 slides
    September 28, 2022

---
+++ tut3/IO/InputOutput/InputOutput/InputOutput.cpp
@@ -0,0 +1,36 @@
+// Input Output tutorial 3
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    //IOSTREAM
+    //float a; //unintilized variable
+    //cout << "Please input a number: " << endl;
+    //cin >> a;
+    //cout << endl << "The inputted number is: " << a << endl;
+
+    //IOManip
+    //cout << left << setw(20) << setfill('_') << "Signature:" << endl;
+
+    //FStream
+    /*ifstream myfile;
+    myfile.open("inputfile.txt");
+    if (!myfile) {
+        cout << "No files opened";
+    }
+    char mytext[20];
+      
+    myfile.getline (mytext, 20);
+     
+    cout << mytext << endl;*/
+
+    
+    
+}
+
+
---
+++ tut3/Vars/Variables/Variables/Variables.cpp
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int main()
+{
+    //Data types
+   /* double a; 
+    cout << "Input a number: ";
+    cin >> a;
+    cout << endl <<"Your number is " << setprecision(18)<< a;*/
+
+    //
+
+}
+
+
