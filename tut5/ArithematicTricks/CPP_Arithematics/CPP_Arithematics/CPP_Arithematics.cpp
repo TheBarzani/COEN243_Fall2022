c5a96f4127c587f6b92365a4a92653057176c1b0
Ismael Barzani <96747803+TheBarzani@users.noreply.github.com>
2022-10-26 16:49:46 -0400

    Tutorial 5 slides and files

---
+++ tut5/ArithematicTricks/CPP_Arithematics/CPP_Arithematics/CPP_Arithematics.cpp
@@ -0,0 +1,15 @@
+// Tutorial 4
+// Author: Ismael Ridha
+// Some arithematic tricks and utilities 
+// 10-05-2022
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main()
+{
+    
+}
---
+++ tut5/Simple loops/Simple loops/Simple loops/Simple loops.cpp
@@ -0,0 +1,70 @@
+// Simple loops.cpp 
+// Tutorial 5
+// Author: Ismael Ridha
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Do...while example
+	/*int i = 11;
+	do {
+		if (i != 10) {
+			cout << i << ", ";
+		}
+		else {
+			cout << i;
+		}
+		i++;
+	} while (i <= 10);
+
+	cout << endl << i;*/
+
+
+	// Simple for loop
+
+	/*for (int i = 0; i <= 10; i++) {
+		cout << i << ", ";
+	}
+	
+	int i =90;
+	cout << i;*/
+
+	// infine while loop
+
+	/*while (true) {
+		cout << 9;
+	}*/
+
+	// infinite for loop v1 
+
+	/*for (;;) {
+		cout << 9; 
+	}*/
+
+	// infinite for loop v2 
+	/*for (int i = 0; i != -1; i++) {
+		cout << i;
+	}*/
+
+	// a grid of numbers using nested loops
+
+	/*for (int i = 0; i < 10; i++) {
+		for (int j = i*10; j < i * 10 + 10 ; j++) {
+			cout << j << " \t";
+		}
+		cout << endl;
+	}*/
+
+	//Arrays
+
+	/*int a = 10;
+	int my_array1[10];
+
+	int my_array2[4] = { 1, 2, 3, 4 };
+	cout << my_array1[0];*/
+
+
+}
+
